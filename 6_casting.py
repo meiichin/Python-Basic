26c9653e4e8371c2c5c39f307275b0584842b7c4
meiichin <meiichan3017@gmail.com>
2019-04-20 12:24:20 +0700

    [Added] casting in py

---
+++ 6_casting.py
@@ -0,0 +1,21 @@
+"""
+There may be times when you want to specify a type on to a variable. This can be done with casting.
+"""
+
+"""
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+"""
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
