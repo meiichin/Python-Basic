f400b3eaa7f1de3e2238bea097f20b7c45d74a10
meiichin <meiichan3017@gmail.com>
2019-04-20 12:24:31 +0700

    [Added] strings in py

---
+++ 7_strings.py
@@ -0,0 +1,17 @@
+'''
+a = "Hello, World!"
+print(a[1])
+
+b = "Hello, World!"
+print(b[2:5])           
+
+print(a.strip()) # The strip() method removes any whitespace from the beginning or the end:
+
+print(len(a)) #The len() method returns the length of a string:
+
+print(a.split(",")) # returns ['Hello', ' World!'] 
+'''
+
+print("Enter your name:")
+x = input()
+print("Hello, ", x)
